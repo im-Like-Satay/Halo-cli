0464a4f6ce3db5fc52e08b0652e9e97499f75aeb
NotDifined <kakak.jiddan@gmail.com>
2026-07-26 06:49:53 +0700

    clean code: delete not important comment from cobra lib

--- cmd/set.go
+++ cmd/set.go
@@ -28,7 +28,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// setCmd represents the set command
 var setCmd = &cobra.Command{
 	Use:   "set [apikey]",
 	Short: "Set Gemini apikey",
@@ -48,14 +47,4 @@ var setCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(setCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// setCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// setCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
